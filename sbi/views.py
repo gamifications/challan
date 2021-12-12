ff0d2de964968d42b937ce608e2b67115481200f
suhailvs <suhailvs@gmail.com>
2021-12-12 12:55:25 +0530

    added static files

--- sbi/views.py
+++ sbi/views.py
@@ -4,20 +4,23 @@ from weasyprint import HTML
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.files import File
-from .models import ChallanFile
+import inflect
+from .models import ChallanFile, Account
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        html = HTML(string=render_to_string('challan_template.html', {'foo': 'bar'}))
+        cfile = ChallanFile.objects.create(
+            amount=100.50
+        )
+        context = {'file': cfile}
+        amount_to_words = inflect.engine()
+        context['amount_in_words'] = amount_to_words.number_to_words(cfile.amount)
+
+        html = HTML(string=render_to_string('challan_template.html', context))
         html.write_pdf(f'{settings.MEDIA_ROOT}/output/challan.pdf')
 
-        cfile = ChallanFile.objects.create(
-                amount=100.50
-            )
-
         cfile.challanfile.save(f'{cfile.id}.pdf', File(open(f'{settings.MEDIA_ROOT}/output/challan.pdf','rb')))
-        # template_data['output_files'].append(output_file)
         return redirect('/')
     
     return render(request,'home.html',{'files':ChallanFile.objects.order_by('-uploading_date')[:5]})
